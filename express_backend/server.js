55c226f3d623a7f923e951c41420e815e2a09503
Your Name <christopher_webdev@outlook.com>
2026-01-27 23:40:05 +0100

    d

--- express_backend/server.js
+++ express_backend/server.js
@@ -176,7 +176,9 @@ app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send('Something broke!');
 });
-
+app.get('/', (req, res) => {
+  res.json({ message: 'Clippapay API is live! 🚀' });
+});
 // 7. Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, '0.0.0.0', () => {
